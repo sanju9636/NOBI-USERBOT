6e97fee3b2531d3dff47e016b2e2f5e0f8a527e1
ᴋᴜɴᴀʟ [AFK] <155554911+VARC9210@users.noreply.github.com>
2024-03-13 22:45:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
 from config import API_ID, API_HASH, SESSIONS
 from pyrogram import Client, idle
 
-
 CLIENTS = []
 
 for SESSION in SESSIONS:
     if SESSION:
         client = Client(
-            session_name=SESSION,
+            name=SESSION,
             api_id=API_ID,
             api_hash=API_HASH,
             plugins=dict(root="STORM"),
         )
         CLIENTS.append(client)
 
-
 if __name__ == "__main__":
-
     for i, CLIENT in enumerate(CLIENTS):
         try:
             CLIENT.start()
             CLIENT.join_chat("STORM_CHATZ")
             CLIENT.join_chat("STORM_TECHH")
-            print(f"ꜱᴛᴏʀᴍ ᴀꜱt {i+1} ʜᴀꜱ ʙᴇᴇɴ ꜱᴛᴀʀᴛᴇᴅ....")
+            print(f"ꜱᴛᴏʀᴍ ᴀꜱᴛ {i+1} ʜᴀꜱ ʙᴇᴇɴ ꜱᴛᴀʀᴛᴇᴅ....")
         except Exception as e:
             print(e)
 
