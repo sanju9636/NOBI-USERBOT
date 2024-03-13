fbc9c17244b9c056c237089661613ee9a2605c63
ᴋᴜɴᴀʟ [AFK] <155554911+VARC9210@users.noreply.github.com>
2024-03-14 01:01:24 +0800

    ꜱ ᴛ ᴏ ʀ ᴍ

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.9.7
+RUN apt update && apt upgrade -y
+RUN apt install python3-pip -y
+RUN apt install ffmpeg -y
+RUN mkdir /app/
+COPY . /app
+WORKDIR /app
+RUN pip3 install --upgrade pip
+RUN pip3 install -U -r requirements.txt
+CMD python3 main.py
---
+++ config.py
@@ -0,0 +1,32 @@
+from os import getenv
+from STORMDB.data import STORMS
+
+
+
+API_ID = int(getenv("API_ID", "25981592"))
+API_HASH = getenv("API_HASH", "709f3c9d34d83873d3c7e76cdd75b866")
+SESSION1 = getenv("SESSION")
+ALIVE_PIC = getenv("ALIVE_PIC", "https://graph.org/file/5d4a2dbf4f196fcdfe4d2.mp4")
+OWNER_ID = int(getenv("OWNER_ID", "6257927828"))
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+
+
+
+SESSION2 = getenv("SESSION2")
+SESSION3 = getenv("SESSION3")
+SESSION4 = getenv("SESSION4")
+SESSION5 = getenv("SESSION5")
+SESSION6 = getenv("SESSION6")
+SESSION7 = getenv("SESSION7")
+SESSION8 = getenv("SESSION8")
+SESSION9 = getenv("SESSION9")
+SESSION10 = getenv("SESSION10")
+
+SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", "6257927828").split(" ")))
+SUDO_USERS.append(OWNER_ID)
+
+for x in STORMS:
+    SUDO_USERS.append(x)
+
+SESSIONS = [SESSION1, SESSION2, SESSION3, SESSION4, SESSION5, SESSION6, SESSION7, SESSION8, SESSION9, SESSION10]
