0ee08201779243dcbf2cd0c95d1962394c9dd6f6
ᴋᴜɴᴀʟ [AFK] <155554911+VARC9210@users.noreply.github.com>
2024-03-14 22:39:25 +0800

    ꜱ ᴛ ᴏ ʀ ᴍ

---
+++ STORM/modules/bot.py
@@ -0,0 +1,39 @@
+import sys
+import datetime
+
+from os import execle, environ
+from config import ALIVE_PIC, SUDO_USERS
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+KEX = f"""ㅤ ꜱᴛᴏʀᴍ ᴜꜱᴇʀʙᴏᴛ ‌🪽
+➖➖➖➖➖➖➖➖➖➖➖➖
+**• ᴘʏᴛʜᴏɴ ᴠᴇʀꜱɪᴏɴ** 🐍: `3.11.3`
+**• ᴜꜱᴇʀʙᴏᴛ ᴠᴇʀꜱɪᴏɴ** ⚙️: `M1.0`
+**• ɢʀᴏᴜᴘ 💫: [ꜱᴛᴏʀᴍ ᴄʜᴀᴛᴢ 🥀](https://t.me/STORM_CHATZ)**
+**• ᴄʜᴀɴɴᴇʟ ✨: [ꜱᴛᴏʀᴍ ᴛᴇᴄʜ 🥀](https://t.me/STORM_TECHH)**
+**• ꜱᴇɴꜱᴇɪ 🫂: [ꜱᴛᴏʀᴍ 🥀](https://t.me/kexx_XD)**
+➖➖➖➖➖➖➖➖➖➖➖➖"""
+
+@Client.on_message(filters.user(SUDO_USERS) & filters.command(["ping"], ["/", ".", "!"]))
+async def ping(_, e: Message):       
+      start = datetime.datetime.now()
+      Fuk = await e.reply("⚡")
+      end = datetime.datetime.now()
+      ms = (end-start).microseconds / 1000
+      await Fuk.edit_text(f"ꜱ ᴛ ᴏ ʀ ᴍ 🥀\nᴛʜᴇ ᴄᴀʟᴍ ʙᴇꜰᴏʀᴇ ᴛʜᴇ ꜱᴛᴏʀᴍ ⚡\n» `{ms} ᴍꜱ`")
+
+@Client.on_message(filters.user(SUDO_USERS) & filters.command(["reboot", "restart"], ["/", ".", "!"]))
+async def restart_bot(_, message: Message):
+    msg = await message.reply("ʀᴇꜱᴛᴀʀᴛɪɴɢ...")
+    args = ["python3", "-m", "STORM"]
+    await msg.edit("ʀᴇꜱᴛᴀʀᴛɪɴɢ...")
+    os.execv(sys.executable, [sys.executable] + args)     
+
+@Client.on_message(filters.user(SUDO_USERS) & filters.command(["alive"], ["/", ".", "!"]))
+async def alive(xspam: Client, msg: Message):
+       if ".jpg" in ALIVE_PIC or ".png" in ALIVE_PIC:
+              await xspam.send_photo(msg.chat.id, ALIVE_PIC, caption=KEX)
+       if ".mp4" in ALIVE_PIC or ".MP4," in ALIVE_PIC:
+              await xspam.send_video(msg.chat.   id, ALIVE_PIC, caption=KEX)    
---
+++ STORM/modules/extra.py
@@ -0,0 +1,57 @@
+import heroku3
+
+from os import getenv
+from config import SUDO_USERS, HELP_PIC, OWNER_ID, HEROKU_APP_NAME, HEROKU_API_KEY
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+FIRST_TEXT = f"""✨ **ʙᴏᴛ ʜᴇʟᴘ** ✨
+
+**[ꜱᴛᴏʀᴍ ᴜꜱᴇʀʙᴏᴛ](https://t.me/Kexx_XD) ʜᴇʟᴘ ᴍᴇɴᴜ** 🥀
+
+**ʜᴇʟᴘ ᴍᴇɴᴜ ᴘᴏᴡᴇʀᴇᴅ ʙʏ [ꜱᴛᴏʀᴍ](https://github.com/VARC9210/STORM-USERBOT)** ✨
+
+**ᴄʜᴀɴɴᴇʟ: [ꜱᴛᴏʀᴍ ᴛᴇᴄʜ 🇮🇳](https://t.me/STORM_TECHH)**
+**ꜱᴜᴘᴘᴏʀᴛ: [ꜱᴛᴏʀᴍ ᴄʜᴀᴛᴢ 🇮🇳](https://t.me/STORM_CHATZ)**
+
+**» ʙᴏᴛ ᴄᴏᴍᴍᴀɴᴅꜱ:** [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://t.me/rasedidstore/1072)
+**» ʀᴀɪᴅ ᴄᴏᴍᴍᴀɴᴅꜱ:** [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://t.me/rasedidstore/1070)
+**» ꜱᴘᴀᴍ ᴄᴏᴍᴍᴀɴᴅꜱ:** [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://t.me/rasedidstore/1069)
+**» ᴅᴍ ᴄᴏᴍᴍᴀɴᴅꜱ:** [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://t.me/rasedidstore/1071)"""
+
+
+@Client.on_message(filters.user(SUDO_USERS) & filters.command(["help"], [".", "!", "/"]))
+async def help(client: Client, message: Message):
+    await client.send_photo(
+        chat_id=message.chat.id,
+        photo=HELP_PIC,
+        caption=FIRST_TEXT
+    )
+
+@Client.on_message(filters.user(OWNER_ID) & filters.command(["sudo"], ["/", ".", "!"]))
+async def add_sudo(_, message: Message):
+       if not message.reply_to_message:
+              await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴜꜱᴇʀ 🙂")
+              return
+       elif HEROKU_APP_NAME is None:
+              await message.reply_text("[HEROKU]:" "\nPlease Setup Your HEROKU_APP_NAME")
+              return
+       elif HEROKU_API_KEY is None:
+              await message.reply_text("[HEROKU]:" "\nPlease Setup Your **HEROKU_API_KEY")
+              return
+       else:
+              heroku = heroku3.from_key(HEROKU_API_KEY)
+              app = heroku.app(HEROKU_APP_NAME)
+
+       ok = await message.reply_text(f"ᴀᴅᴅɪɴɢ ᴜꜱᴇʀ ᴀꜱ ꜱᴜᴅᴏ ⏳...")
+       heroku_var = app.config()
+
+       sudousers = getenv("SUDO_USERS")
+       target = message.reply_to_message.from_user.id
+       if len(sudousers) > 0:
+              newsudo = f"{sudousers} {target}"
+       else:
+              newsudo = f"{target}"
+       await ok.edit(f"ɴᴇᴡ ꜱᴜᴅᴏ ᴜꜱᴇʀ `{target}`\nʀᴇsᴛᴀʀᴛɪɴɢ ⏳...")
+       heroku_var["SUDO_USERS"] = newsudo     
