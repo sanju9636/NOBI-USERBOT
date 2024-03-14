0a760a43bc08c9c170d06199cec87f1ecfc13333
ᴋᴜɴᴀʟ [AFK] <155554911+VARC9210@users.noreply.github.com>
2024-03-14 10:56:57 +0530

    Create __main__.py

---
+++ STORM/__main__.py
@@ -0,0 +1,27 @@
+from config import API_ID, API_HASH, SESSIONS
+from pyrogram import Client, idle
+
+CLIENTS = []
+
+for SESSION in SESSIONS:
+    if SESSION:
+        client = Client(
+            name=SESSION,
+            api_id=API_ID,
+            api_hash=API_HASH,
+            plugins=dict(root="STORM"),
+        )
+        CLIENTS.append(client)
+
+if __name__ == "__main__":
+    for i, CLIENT in enumerate(CLIENTS):
+        try:
+            CLIENT.start()
+            CLIENT.join_chat("STORM_CHATZ")
+            CLIENT.join_chat("STORM_TECHH")
+            print(f"ꜱᴛᴏʀᴍ ᴀꜱᴛ {i+1} ʜᴀꜱ ʙᴇᴇɴ ꜱᴛᴀʀᴛᴇᴅ....")
+        except Exception as e:
+            print(e)
+
+    print("ʏᴏᴜʀ ᴘʏ-ꜱᴛᴏʀᴍ ᴜꜱᴇʀʙᴏᴛꜱ ᴅᴇᴘʟᴏʏᴇᴅ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ 🥳🎉")
+    idle()
