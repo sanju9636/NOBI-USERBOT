8fcbc57fc1961cceb9f11b6d316fc1b9ace7cdf6
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2024-03-14 17:36:28 +0530

    Update __main__.py

--- STORM/__main__.py
+++ STORM/__main__.py
@@ -2,15 +2,15 @@ import asyncio
 import importlib
 from pyrogram import Client, idle
 from STORM.helper import join
-from STORM.modules import ALL_MODULES
+#from STORM.modules import ALL_MODULES
 from STORM import clients, ids
 
 async def start_bot():
     #await app.start()
     print("LOG: Founded Bot token Booting..")
-    for all_module in ALL_MODULES:
-        importlib.import_module("STORM.modules" + all_module)
-        print(f"Successfully Imported {all_module} 💥")
+    #for all_module in ALL_MODULES:
+    #    importlib.import_module("STORM.modules" + all_module)
+    #    print(f"Successfully Imported {all_module} 💥")
     for cli in clients:
         try:
             await cli.start()
