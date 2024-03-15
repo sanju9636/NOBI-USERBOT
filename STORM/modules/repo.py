319714bd59523f977f028227949a554e800f2258
ᴋᴜɴᴀʟ [AFK] <155554911+VARC9210@users.noreply.github.com>
2024-03-15 22:46:09 +0530

    Create repo.py

---
+++ STORM/modules/repo.py
@@ -0,0 +1,15 @@
+from pyrogram import Client, filters
+
+@Client.on_message(filters.user(OWNER_ID) & filters.command(["repo"], ["."]))
+async def repo(client, message):
+    msg = f"""
+    ** ꜱᴛᴏʀᴍ ᴜꜱᴇʀʙᴏᴛ **
+
+    • **ɢɪᴛʜᴜʙ** » [click here](https://github.com/VARC9210/STORM-USERBOT) 
+    • **ꜱᴜᴘᴘᴏʀᴛ** » [click here](https://t.me/STORM_CHATZ) 
+    • **ᴜᴘᴅᴀᴛᴇꜱ** » [click here](https://t.me/STORM_TECHH)
+    • **ᴅᴇᴠᴇʟᴏᴘᴇʀ** » [click here](https://t.me/kexx_xd)
+    
+    **ʙʏ @kexx_xd**
+    """
+    await message.edit(msg)
