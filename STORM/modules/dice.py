e8607dcb90b335393e9eda49f1ae478c8d5512fb
ᴋᴜɴᴀʟ [AFK] <155554911+VARC9210@users.noreply.github.com>
2024-03-16 21:28:40 +0530

    Delete STORM/modules/dice.py

--- STORM/modules/dice.py
+++
@@ -1,8 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message
-import asyncio
-from config import SUDO_USERS
-
-@Client.on_message(filters.user(SUDO_USERS) & filters.command(["dice"], ["."]))
-async def dice(_, e: Message):       
-      Fuk = await e.reply("🎲")
