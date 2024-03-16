c2eb187e17dd5a314e85f29d9bcf829ebe6cecdb
ᴋᴜɴᴀʟ [AFK] <155554911+VARC9210@users.noreply.github.com>
2024-03-16 09:45:22 +0530

    Update heart.py

--- STORM/modules/heart.py
+++ STORM/modules/heart.py
@@ -1,8 +1,9 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 import asyncio
+from config import SUDO_USERS
 
-@Client.on_message(filters.user(OWNER_ID) & filters.command(["heart", "love"], ["."]))
+@Client.on_message(filters.user(SUDO_USERS) & filters.command(["heart", "love"], ["."]))
 async def hearts(client: Client, message: Message):
     await message.edit("❤️")
     await asyncio.sleep(0.5)
