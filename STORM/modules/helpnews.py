3b5a1e56eabfa8b1341c4b10f579999d920e08e5
ᴋᴜɴᴀʟ [AFK] <155554911+VARC9210@users.noreply.github.com>
2024-03-16 18:16:05 +0530

    Create helpnews.py

---
+++ STORM/modules/helpnews.py
@@ -0,0 +1,21 @@
+from config import SUDO_USERS, HELP_PIC
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+hl = "."
+
+NEWS_TEXT = f"""
+**ɴᴇᴡꜱ ᴄᴏᴍᴍᴀɴᴅꜱ**
+
+• `{hl}news` » ᴛᴏ ɢᴇᴛ ᴛᴏᴘ 5 ʜᴇᴀᴅʟɪɴᴇꜱ ᴏꜰ ɴᴇᴡꜱ....
+
+• `{hl}weather (ʏᴏᴜʀ ᴄɪᴛʏ)` » ᴛᴏ ɢᴇᴛ ᴡᴇᴀᴛʜᴇʀ ɪɴꜰᴏʀᴍᴀᴛɪᴏɴ....
+"""
+
+@Client.on_message(filters.user(SUDO_USERS) & filters.command(["help news"], ["."]))
+async def help(xspam: Client, msg: Message):
+       if ".jpg" in HELP_PIC or ".png" in HELP_PIC:
+              await xspam.send_photo(msg.chat.id, HELP_PIC, caption=NEWS_TEXT)
+       if ".mp4" in HELP_PIC or ".MP4," in HELP_PIC:
+              await xspam.send_video(msg.chat.   id, HELP_PIC, caption=NEWS_TEXT) 
