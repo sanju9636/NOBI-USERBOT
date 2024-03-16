6d4aabb2445d8c0cb83b10880810c419d37a70d8
ᴋᴜɴᴀʟ [AFK] <155554911+VARC9210@users.noreply.github.com>
2024-03-16 20:04:03 +0530

    Update truth.py

--- STORM/modules/truth.py
+++ STORM/modules/truth.py
@@ -1,3 +1,13 @@
+import asyncio
+
+from random import choice
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from STORMDB.data import STORMS
+from STORMDB.truthDB import TRUTH
+from config import OWNER_ID, SUDO_USERS
+
 @Client.on_message(filters.user(SUDO_USERS) & filters.command(["truth"], ["."]))
 async def truth(xspam: Client, e: Message):
       kex = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 2)
