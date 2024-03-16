729628b0c5372e5bfa9c135894558f108466fd65
ᴋᴜɴᴀʟ [AFK] <155554911+VARC9210@users.noreply.github.com>
2024-03-16 18:14:46 +0530

    Create helplove.py

---
+++ STORM/modules/helplove.py
@@ -0,0 +1,27 @@
+from config import SUDO_USERS, HELP_PIC
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+hl = "."
+
+LOVE_TEXT = f"""
+**ʟᴏᴠᴇ ꜱʜᴏᴡᴇʀ ᴄᴏᴍᴍᴀɴᴅꜱ**
+
+• `{hl}lover` » ᴄʜᴇᴄᴋ ʙʏ ʏᴏᴜʀꜱᴇʟꜰ....
+
+• `{hl}flirt` » ᴄʜᴇᴄᴋ ʙʏ ʏᴏᴜʀꜱᴇʟꜰ....
+
+• `{hl}hflirt` » ᴄʜᴇᴄᴋ ʙʏ ʏᴏᴜʀꜱᴇʟꜰ....
+
+• `{hl}loveraid` » ᴄʜᴇᴄᴋ ʙʏ ʏᴏᴜʀꜱᴇʟꜰ....
+
+• `{hl}sraid` » ᴄʜᴇᴄᴋ ʙʏ ʏᴏᴜʀꜱᴇʟꜰ....
+"""
+
+@Client.on_message(filters.user(SUDO_USERS) & filters.command(["help love"], ["."]))
+async def help(xspam: Client, msg: Message):
+       if ".jpg" in HELP_PIC or ".png" in HELP_PIC:
+              await xspam.send_photo(msg.chat.id, HELP_PIC, caption=LOVE_TEXT)
+       if ".mp4" in HELP_PIC or ".MP4," in HELP_PIC:
+              await xspam.send_video(msg.chat.   id, HELP_PIC, caption=LOVE_TEXT)
