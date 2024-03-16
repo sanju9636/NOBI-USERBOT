5989bf0ef584f7e81f5e6fc0f0404b3c829d8c3a
ᴋᴜɴᴀʟ [AFK] <155554911+VARC9210@users.noreply.github.com>
2024-03-16 09:44:04 +0530

    Update chance.py

--- STORM/modules/chance.py
+++ STORM/modules/chance.py
@@ -1,7 +1,8 @@
 from pyrogram import Client, filters
 import random
+from config import SUDO_USERS
 
-@Client.on_message(filters.user(OWNER_ID) & filters.command(["chance"], ["."]))
+@Client.on_message(filters.user(SUDO_USERS) & filters.command(["chance"], ["."]))
 async def chance(client, message):
     text = message.text.split(hl + "chance ", maxsplit=1)[1]
     await message.edit(f"{text}\n\nᴄʜᴀɴᴄᴇ » {random.randint(1, 100)}%")
