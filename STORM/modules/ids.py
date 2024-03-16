c52c526a893824053c0bba876172cef9186c0f57
ᴋᴜɴᴀʟ [AFK] <155554911+VARC9210@users.noreply.github.com>
2024-03-16 09:45:50 +0530

    Update ids.py

--- STORM/modules/ids.py
+++ STORM/modules/ids.py
@@ -1,6 +1,7 @@
 from pyrogram import Client, filters
+from config import SUDO_USERS
 
-@Client.on_message(filters.user(OWNER_ID) & filters.command(["id"], ["."]))
+@Client.on_message(filters.user(SUDO_USERS) & filters.command(["id"], ["."]))
 async def find_id(client, message):
     if message.reply_to_message is None:
         await message.edit(f"ᴄʜᴀᴛ ɪᴅ » `{message.chat.id}`")
