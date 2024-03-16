3483b71a45191290254335f88e0e09c4f8c2ba29
ᴋᴜɴᴀʟ [AFK] <155554911+VARC9210@users.noreply.github.com>
2024-03-16 18:15:30 +0530

    Create helpfun.py

---
+++ STORM/modules/helpfun.py
@@ -0,0 +1,33 @@
+from config import SUDO_USERS, HELP_PIC
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+hl = "."
+
+FUN_TEXT = f"""
+**ꜰᴜɴ ᴄᴏᴍᴍᴀɴᴅꜱ**
+
+• `{hl}lover` » ᴄʜᴇᴄᴋ ʙʏ ʏᴏᴜʀꜱᴇʟꜰ....
+
+• `{hl}stupid` » ᴄʜᴇᴄᴋ ʙʏ ʏᴏᴜʀꜱᴇʟꜰ....
+
+• `{hl}sex` » ᴄʜᴇᴄᴋ ʙʏ ʏᴏᴜʀꜱᴇʟꜰ....
+
+• `{hl}chance` » ᴄʜᴇᴄᴋ ʙʏ ʏᴏᴜʀꜱᴇʟꜰ....
+
+• `{hl}kiss` » ᴄʜᴇᴄᴋ ʙʏ ʏᴏᴜʀꜱᴇʟꜰ....
+
+• `{hl}dare` » ᴄʜᴇᴄᴋ ʙʏ ʏᴏᴜʀꜱᴇʟꜰ....
+
+• `{hl}truth` » ᴄʜᴇᴄᴋ ʙʏ ʏᴏᴜʀꜱᴇʟꜰ....
+
+• `{hl}emoji` <ᴄᴏᴜɴᴛ> <ʀᴇᴘʟʏ ᴏʀ ᴜꜱᴇʀɴᴀᴍᴇ> » ᴄʜᴇᴄᴋ ʙʏ ʏᴏᴜʀꜱᴇʟꜰ....
+"""
+
+@Client.on_message(filters.user(SUDO_USERS) & filters.command(["help fun"], ["."]))
+async def help(xspam: Client, msg: Message):
+       if ".jpg" in HELP_PIC or ".png" in HELP_PIC:
+              await xspam.send_photo(msg.chat.id, HELP_PIC, caption=FUN_TEXT)
+       if ".mp4" in HELP_PIC or ".MP4," in HELP_PIC:
+              await xspam.send_video(msg.chat.   id, HELP_PIC, caption=FUN_TEXT) 
