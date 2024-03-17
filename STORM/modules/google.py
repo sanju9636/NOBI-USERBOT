e1a673509c5506be193a3db6ffc5705621d9afda
ᴋᴜɴᴀʟ [AFK] <155554911+VARC9210@users.noreply.github.com>
2024-03-17 15:35:51 +0530

    Update google.py

--- STORM/modules/google.py
+++ STORM/modules/google.py
@@ -3,27 +3,24 @@ from pyrogram.types import Message
 from googlesearch import search
 from config import SUDO_USERS
 # Function to perform a Google search and return top search results
+
 def google_search(query, num_results=5):
     results = []
-    for idx, result in enumerate(search(query, num_results=num_results, pause=2), start=1):
+    for result in search(query, num_results=num_results, stop=num_results, pause=2):
         results.append(result)
-        if idx >= num_results:
-            break
     return results
 
 # Define the football command handler
 @Client.on_message(filters.user(SUDO_USERS) & filters.command(["google"], ["."]))
-async def google(client, message: Message):
-    # Extract the search query
-    query = message.text.split(".", maxsplit=1)[1].strip()
-
+async def google(client, message):
+    # Extract the search query from the message
+    query = message.text.split(".google", maxsplit=1)[1]
+    
     # Perform Google search
     search_results = google_search(query)
-
-    # Prepare the response message
+    
+    # Prepare and send the response message
     response = "**ᴛᴏᴘ ꜱᴇᴀʀᴄʜ ʀᴇꜱᴜʟᴛꜱ**:\n"
     for idx, result in enumerate(search_results, start=1):
         response += f"{idx}. {result}\n"
-
-    # Send the response
     await message.reply(response)
