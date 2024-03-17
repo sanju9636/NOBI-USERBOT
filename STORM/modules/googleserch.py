849031557623e20b42c8f8124612b995935806e7
ᴋᴜɴᴀʟ [AFK] <155554911+VARC9210@users.noreply.github.com>
2024-03-17 16:35:10 +0530

    Update googleserch.py

--- STORM/modules/googleserch.py
+++ STORM/modules/googleserch.py
@@ -1,18 +1,51 @@
-import openai
 from pyrogram import Client, filters
-from config import SUDO_USERS, OPENAIKEY
+from config import SUDO_USERS
+from bs4 import BeautifulSoup
+from googlesearch import search
+import requests
+from pyrogram.types import Message
 
-openai.api_key = OPENAIKEY
-@Client.on_message(filters.user(SUDO_USERS) & filters.command(["google"], ["."]))
-async def google(client, message):
-    prompt = message.text.split(".google ", maxsplit=1)[1]
-    try:
-        response = openai.Completion.create(
-            engine="gpt-3.5-turbo-instruct",
-            prompt=prompt,
-            max_tokens=100
-        )
-        completion_text = response.choices[0].text.strip()
-        await message.reply(completion_text)
-    except Exception as e:
-        await message.reply(f"ᴀɴ ᴇʀʀᴏʀ ᴏᴄᴄᴜʀʀᴇᴅ: {e}")
+def googlesearch(query):
+    co = 1
+    returnquery = {}
+    for j in search(query, tld="co.in", num=10, stop=10, pause=2):
+        url = str(j)
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        metas = soup.find_all("meta")
+        site_title = None
+        for title in soup.find_all("title"):
+            site_title = title.get_text()
+        metadeta = [
+            meta.attrs["content"]
+            for meta in metas
+            if "name" in meta.attrs and meta.attrs["name"] == "description"
+        ]
+        returnquery[co] = {"title": site_title, "metadata": metadeta, "url": j}
+        co += 1
+    return returnquery
+
+# Define the gs command handler
+@Client.on_message(filters.command(["gs", "google"], ".") & filters.me)
+async def gs(client: Client, message: Message):
+    Man = await message.edit("ᴘʀᴏᴄᴇꜱꜱɪɴɢ...")
+    msg_txt = message.text
+    returnmsg = ""
+    query = None
+    if " " in msg_txt:
+        query = msg_txt[msg_txt.index(" ") + 1:]
+    else:
+        await Man.edit("ɢɪᴠᴇ ᴀ Qᴜᴇʀʏ ᴛᴏ ꜱᴇᴀʀᴄʜ")
+        return
+    results = googlesearch(query)
+    for i in range(1, 10):
+        presentquery = results.get(i, {})
+        presenttitle = presentquery.get("title", "")
+        presentmeta = presentquery.get("metadata", [])
+        presenturl = presentquery.get("url", "")
+        if not presentmeta:
+            presentmeta = ""
+        else:
+            presentmeta = presentmeta[0]
+        returnmsg += f"[{str(presenttitle)}]({str(presenturl)})\n{str(presentmeta)}\n\n"
+    await Man.edit(returnmsg)
